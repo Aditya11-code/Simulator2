ae57b9d0ce06ff2e43a750e314fd7aa0e03c5408
Aditya Kesharwani <workforadi11@gmail.com>
2020-09-12 01:00:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,62 @@
-function openNav() {
-document.getElementById("mySidepanel").style.width = "250px";
-}
+var synt = document.querySelector(".syntax-para");
+var define = document.querySelector(".defi-para");
+var fChart = document.querySelector(".flowchart-para");
+var defBtn = document.getElementById('define');
+var synBtn = document.getElementById('syntax');
+var fcBtn = document.getElementById('fchart');
 
-function closeNav() {
-document.getElementById("mySidepanel").style.width = "0";
+function definition() {
+  if(define.style.display === "block"){
+    define.style.display = "none";
+    defBtn.style.background = "none";
+    defBtn.style.color = "#3072b3";
+  }else{
+    define.style.display = "block";
+    defBtn.style.backgroundImage = "linear-gradient(#599bdc, #3072b3)";
+    defBtn.style.color = "white";
+    synt.style.display = "none";
+    define.style.display = "block";
+    fChart.style.display = "none";
+    synBtn.style.background = "none";
+    synBtn.style.color = "#3072b3";
+    fcBtn.style.background = "none";
+    fcBtn.style.color = "#3072b3";
+  }
+
+}
+function syntax() {
+  if(synt.style.display === "block"){
+    synt.style.display = "none";
+    synBtn.style.background = "none";
+    synBtn.style.color = "#3072b3";
+  }
+  else{
+    synt.style.display = "block";
+    define.style.display = "none";
+    fChart.style.display = "none";
+    defBtn.style.background = "none";
+    defBtn.style.color = "#3072b3";
+    synBtn.style.backgroundImage = "linear-gradient(#599bdc, #3072b3)";
+    synBtn.style.color = "#fff";
+    fcBtn.style.background = "none";
+    fcBtn.style.color = "#3072b3";
+  }
+}
+function flowchart() {
+  if(fChart.style.display === "block"){
+    fChart.style.display = "none";
+    fcBtn.style.background = "none";
+    fcBtn.style.color = "#3072b3";
+  }
+  else{
+    synt.style.display = "none";
+    define.style.display = "none";
+    fChart.style.display = "block";
+    defBtn.style.background = "none";
+    defBtn.style.color = "#3072b3";
+    synBtn.style.background = "none";
+    synBtn.style.color = "#3072b3";
+    fcBtn.style.backgroundImage = "linear-gradient(#599bdc, #3072b3)";
+    fcBtn.style.color = "#fff";
+  }
 }
